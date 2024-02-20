3338b212f2b0d96fbbba287323210e5e0d4b4de8
mariairmendes16 <160590866+mariairmendes16@users.noreply.github.com>
2024-02-20 09:57:20 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,10 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
+import numpy as np
 import pandas as pd
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
 
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-
-st.text_input("Your name", key="name")
-
-# You can access the value at any point with:
-st.session_state.name
+    chart_data
