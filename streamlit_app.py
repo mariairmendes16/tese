5791bf46b4997fe5256135b1c5ce942517932124
mariairmendes16 <160590866+mariairmendes16@users.noreply.github.com>
2024-02-20 09:56:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,9 @@ st.write(pd.DataFrame({
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
