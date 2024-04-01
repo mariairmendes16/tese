5193620cac489bb0a2e6b8e8f38f6e6d86015966
mariairmendes16 <160590866+mariairmendes16@users.noreply.github.com>
2024-04-01 14:40:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,8 +96,8 @@ if add_third_color:
 
 with col2:
     email_category = st.selectbox("Email Category", ("Apology","Birthday Email","Booking Cancellation","Booking Confirmation","Check-Out Reminder", "Double Opt-In", "F&B", "Feedback Request" , "Informative", "Invitation", "Invoice Email", "Legal Updates",  "Loyalty Offer", "Loyalty Program Presentation", "Mid-stay", "New Level of Loyalty Program", "New Loyalty Member", "Newsletter", "Pre-arrival",  "Spa", "Special Occasions", "Special Offers", "Stay Anniversary", "Welcome Email"), index=None, placeholder="Select email category...")
-    if email_category == "NONE":
-    st.error("Please choose an email category.", 🤖)
+        if email_category == "NONE":
+            st.error("Please choose an email category.", icon="🤖")
     speech_tone = st.selectbox("Speech Tone", ("🤝 Professional", "😊 Friendly", "🎉 Celebratory", "🙏 Apologetic", "💡 Informative", "🌟 Persuasive", "💌 Welcoming", "🥳 Excited", "🛎️ Urgent", "🤗 Appreciative"), index=None, placeholder="Select speech tone...")
 email_description = st.text_area("Small Description (Max 200 Characters)", max_chars=200, placeholder="Please insert email description here.")
 
